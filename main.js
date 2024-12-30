486f1af890528b0c42d97869de9be70269c5838a
abdalluhyaseen <abdalluhyaseen22@icloud.com>
2024-12-30 22:31:20 +0300

    finish javascript2

---
+++ main.js
@@ -0,0 +1,108 @@
+
+var age= 27;
+if (age > 18){
+    document.write("You are an adult" + "<br>");
+}
+//#####################################################
+// task 2
+ var score = 45;
+
+if (score <= 50){
+    document.write("You failed the test" + "<br>");
+}
+//#####################################################
+// task 3
+var name = "John" ;
+if (name == "John"){
+    document.write("Hello, John" + "<br>");
+}
+//#####################################################
+// task 4
+var day = "Monday " ;
+if (day !== "Saturday" && day !== "Sunday"){
+    document.write("It's a weekday" + "<br>");
+}
+//#####################################################
+// task 5
+var num = 4 ;
+if (num %2 == 0){
+    document.write("The number is even" + "<br>");
+}
+//#####################################################
+// task 6
+var char = "a" ; 
+if (char = "letter" ){
+    document.write("It's a letter" + "<br>");
+}
+//#####################################################
+// task 7
+var list = [1, 2, 3];
+if (list= "array"){
+    document.write("It's an array" + "<br>");
+}
+//#####################################################
+// task 8
+var x = 5 ;
+if (x = "positive number"){
+    document.write("x is a positive number" + "<br>");
+}
+//#####################################################
+// task 9
+var y = -5 ;
+if (y = "negative number"){
+    document.write("x is a negative number" + "<br>");
+}
+//#####################################################
+// task 10
+var z = 9 ;
+if (z = "multiple of 3"){
+    document.write("z is a multiple of 3 " + "<br>");
+}
+//#####################################################
+// task 11
+var GPA = "a" ;
+switch (GPA){   
+case "A":
+case "a":
+document.write("Excellent" + "<br>");
+case "B":
+case "b":
+document.write("Very Good" + "<br>");
+case "C":
+case "c":
+document.write("Good" + "<br>");
+case "F":
+case "f":
+ document.write("Failed" + "<br>");
+ default:
+ document.write("a proper message" + "<br>");
+}
+//#####################################################
+// task 12
+let month = "January"; 
+if (month === "December" , month === "January" || month === "February"){
+    document.write("a winter month"+"<br>" )}
+else if (month === "March" || month === "April" || month === "May") {
+    document.write("a spring month"+"<br>" )}
+ else if (month === "June" || month === "July" || month === "August"){
+     document.write("a summer month"+"<br>") }  
+  else if (month === "September" || month === "October" || month === "November"){  
+     document.write("autumn month"+"<br>")}
+  else{
+    document.write("unknown"+"<br>")
+}
+
+//#####################################################
+// task 13
+var password = "mysecureoass";
+if (password.length >= 8){
+    document.write("Your password is strong" + "<br>");
+}
+else{
+    document.write("Your password is shprt" + "<br>");
+}
+//#####################################################
+// task 14
+
+
+
